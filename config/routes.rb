4247073eb072a3be4263e1ae6319d9538321cf63
Calvin <xcalvin.zhao@gmail.com>
2017-08-05 23:30:52 +0800

    8-1 情境准备

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events
-
+  
   namespace :admin do
     root "events#index"
     resources :events
+      resources :users
   end
 
   root "events#index"
