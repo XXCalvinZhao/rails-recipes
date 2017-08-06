cec67ce56f4c5fc0589458db1fb319f711351f4d
Calvin <xcalvin.zhao@gmail.com>
2017-08-06 16:35:14 +0800

    10-2 嵌套(Nested)表单UI

--- app/models/event.rb
+++ app/models/event.rb
@@ -24,4 +24,7 @@ class Event < ApplicationRecord
 
  has_many :tickets, :dependent => :destroy
 
+ has_many :tickets, :dependent => :destroy, :inverse_of  => :event
+ accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
+
 end
